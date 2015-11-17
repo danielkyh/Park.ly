24d251b57a2de0890f5a848ed7ded0b0b59dd10b
Daniel Huang <danielh714@gmail.com>
2015-11-16 23:28:08 -0800

    change cors route

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var models = require('../models');
 var bcrypt = require('bcrypt');
 
 router.use(function(req, res, next) {
-   res.header('Access-Control-Allow-Origin', 'http://murmuring-caverns-3836.herokuapp.com');
+   res.header('Access-Control-Allow-Origin', 'http://parklyclient.herokuapp.com');
    next();
 })
 // User creation route
